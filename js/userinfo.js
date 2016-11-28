0a3b49a66b13eddf394eca2ff43d8649ae867915
efectoTam <tam.luque@gmail.com>
2016-11-28 12:38:45 -0300

    Ahora se puede editar correctamente la imagen de perfil y los datos del usuario

--- js/userinfo.js
+++ js/userinfo.js
@@ -1,22 +1,38 @@
-$(document).ready(function (){
-	$(".retroceder").click(function(){
-		window.location.href = "index.html";
-	});
+$(function() {
+ $('#file-input').change(function(e) {
+      addImage(e); 
+  });
+  function addImage(e){
+    var file = e.target.files[0],
+    imageType = /image.*/;
+    if (!file.type.match(imageType))
+      return;
+      var reader = new FileReader();
+      reader.onload = fileOnload;
+      reader.readAsDataURL(file);
+  }
+  function fileOnload(e) {
+    var result=e.target.result;
+    $('#nuevaImagenUno').attr("src",result);
+    $('#nuevaImagenDos').attr("src",result);
+  }
+});
 
-	var caracteresNombre = 30;
-	$("#name").keyup(function(){
-    	if($(this).val().length > caracteresNombre){
-       		$(this).val($(this).val().substr(0, caracteresNombre));
-		}
-	});
+$(document).ready(function(){
+  $(".paraEditar").hide();
+  $(".editarProfile").click(function(){
+    $(".perfilEditado").hide();
+    $(".paraEditar").show();
+  });
 
-	var caracteresEmail = 50;
-	$("#email").keyup(function(){
-    	if($(this).val().length > caracteresEmail){
-       		$(this).val($(this).val().substr(0, caracteresEmail));
-		}
-	});
-
-
-
+  $(".botonGuardar").click(function(){
+    var textoCasa= $(".textareaCasita").val();
+    $(".pCasita").text(textoCasa);
+    var textoNota= $(".textareaNotaMusical").val();
+    $(".pNotaMusical").text(textoNota);
+    var textoUsuario= $(".textareaDeUsuario").val();
+    $(".pDeUsuario").text(textoUsuario);
+    $(".perfilEditado").show();
+    $(".paraEditar").hide();
+  });
 });
