8b3a5090973f95a35d0fcbe315cdd40f78ffdd17
efectoTam <tam.luque@gmail.com>
2016-11-29 14:40:32 -0300

    Varios cambios

--- js/createuser.js
+++ js/createuser.js
@@ -1,20 +1,6 @@
 $(document).ready(function (){
-	$(".retroceder").click(function(){
-		window.location.href = "index.html";
-	});
-
-	var caracteresNombre = 30;
-	$("#name").keyup(function(){
-    	if($(this).val().length > caracteresNombre){
-       		$(this).val($(this).val().substr(0, caracteresNombre));
-		}
-	});
-
-	var caracteresEmail = 50;
-	$("#email").keyup(function(){
-    	if($(this).val().length > caracteresEmail){
-       		$(this).val($(this).val().substr(0, caracteresEmail));
-		}
+	$(".aSignup").click(function(){
+		window.location.href = "signup.html";
 	});
 
 	$(".nextUsuario").click(function(){
--- js/signup.js
+++ js/signup.js
@@ -1,25 +1,11 @@
 $(document).ready(function (){
-
-	//Como inicia automáticamente en Chile, se limitan los números para el teléfono a penas se abre la página.
-	var caracteres = 9;
-	$("#exampleInputAmount").keyup(function(){
-    	if($(this).val().length > caracteres){
-       		$(this).val($(this).val().substr(0, caracteres));
-		}
-	});
 	//Cuando se hace click en cada bandera
 	$('.mexico').click(function (){
 		$(".textareaTel").val("");
 		$(".tamanoBanderas").attr("src","img/MX.png");
 		$(".prefijoTel").text("+52");
 		$(".textareaTel").attr("placeholder", "01 222 123 456");
-
-		var caracteres = 11;
-		$("#exampleInputAmount").keyup(function(){
-    		if($(this).val().length > caracteres){
-        		$(this).val($(this).val().substr(0, caracteres));
-			}
-		});
+		$(".textareaTel").attr("maxlength", "11");
 	});
 
 	$('.peru').click(function (){
@@ -27,27 +13,15 @@ $(document).ready(function (){
 		$(".tamanoBanderas").attr("src","img/PE.png");
 		$(".prefijoTel").text("+51");
 		$(".textareaTel").attr("placeholder", "(01) 1234567");
-
-		var caracteres = 9;
-		$("#exampleInputAmount").keyup(function(){
-    		if($(this).val().length > caracteres){
-        		$(this).val($(this).val().substr(0, caracteres));
-			}
-		});
+		$(".textareaTel").attr("maxlength", "9");
 	});
 
 	$('.estadosUnidos').click(function (){
 		$(".textareaTel").val("");
 		$(".tamanoBanderas").attr("src","img/US.png");
 		$(".prefijoTel").text("+1");
-		$(".textareaTel").attr("placeholder", "(201) 555-5555s");
-
-		var caracteres = 10;
-		$("#exampleInputAmount").keyup(function(){
-    		if($(this).val().length > caracteres){
-        		$(this).val($(this).val().substr(0, caracteres));
-			}
-		});
+		$(".textareaTel").attr("placeholder", "(201) 555-5555");
+		$(".textareaTel").attr("maxlength", "11");
 	});
 
 	$('.chile').click(function (){
@@ -55,13 +29,7 @@ $(document).ready(function (){
 		$(".tamanoBanderas").attr("src","img/CL.png");
 		$(".prefijoTel").text("+56");
 		$(".textareaTel").attr("placeholder", "(2) 2123 4567");
-
-		var caracteres = 9;
-		$("#exampleInputAmount").keyup(function(){
-    		if($(this).val().length > caracteres){
-        		$(this).val($(this).val().substr(0, caracteres));
-			}
-		});
+		$(".textareaTel").attr("maxlength", "9");
 	});
 
 	//El botón para retroceder conecta con Home
@@ -74,24 +42,44 @@ $(document).ready(function (){
         this.value = (this.value + '').replace(/[^0-9]/g, '');
     });
 
-    //Se genera un código aleatorio cuando se hace click en next
-	$(".nextNumerico").click(function(){
-		function numeroAleatorio(min, max) {
-  			alert("LAB-"+Math.round(Math.random() * (max - min) + min));
-		}
-		numeroAleatorio(100, 999);
-
-		var numeroUsuario= $(".textareaTel").val();
-		var prefijo = $(".prefijoTel").text();
-
-		$(".telUsuario:eq(0)").append("Enter the code sent to"+ " " + prefijo+" "+numeroUsuario);
+  	$(".verificarCode").hide();
+  	//Solo aparece el código
+  	$(".nextNumerico").click(function(){
+  		var telefono = $('.textareaTel').val();
+		if(telefono==null || telefono.length==0 || /^\s+$/.test(telefono)){
+			alert("Ingrese su teléfono");
+			return false;
+		} else{
+			var numeroUsuario= $(".textareaTel").val();
+			var prefijo = $(".prefijoTel").text();
+			$(".telUsuario:eq(0)").append("Enter the code sent to"+ " " + prefijo+" "+numeroUsuario);
+			function numeroAleatorio(min, max) {
+	  			var numAlea= ("LAB-"+Math.round(Math.random() * (max - min) + min));
+	  			alert(numAlea);
+	  			localStorage.setItem("codigoDado", numAlea);
+			}
+			numeroAleatorio(100, 999);
+    		$(".ocultarPorCodigo").hide();
+    		$(".verificarCode").show();
+    	}
+  	});
+  	$(".retrocederTelefono").click(function(){
+  		$(".verificarCode").hide();
+		$(".ocultarPorCodigo").show();	
 	});
 
-  	$(".verificarCode").hide();
-  	$(".nextNumerico").click(function(){
-    	$(".ocultarPorCodigo").hide();
-    	$(".verificarCode").show();
+  	$(".nextVerificacion").click(function(){
+  		var numeroPorUsuario = $(".ingresoCodigo").val();
+  		var numeroGuardado = localStorage.getItem("codigoDado");
+  		if (numeroPorUsuario===numeroGuardado){
+			window.location.href = "createuser.html";
+  		}else{
+  			alert("Error en el código.");
+  			return false;
+  		}
+
   	});
+
 
 
 });
